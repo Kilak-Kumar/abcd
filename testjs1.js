1e300dfd9f7a94558a6969e2232ebf9918925944
Kilak-Kumar <kilakkumar@gmail.com>
2024-02-26 13:14:42 +0530

    Javascript file

---
+++ testjs1.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+
+
+<!DOCTYPE html >
+<html>
+    <head>
+        <H1>
+        <p> This is first JS Program </p>
+        </H1>
+    </head>
+    <body>
+        <button type="button"
+            onclick="document.getElementById('demo').innerHTML = Date()">
+            Click me to display Date and Time.</button>
+
+        <p id="demo"></p>
+    </body>
+
+</html>
